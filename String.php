a43ea38830e9e9582cb75d62900d6005661c96fa
JohanesKristiadi <johanes.kristiadi@student.umn.ac.id>
2023-01-29 17:56:25 +0700

    menambahkan string.php

---
+++ String.php
@@ -0,0 +1,19 @@
+<?php
+
+echo 'Nama : ';
+echo 'Johanes Kristiadi';
+
+// Double tab
+
+echo "/n";
+echo "Name : ";
+echo "Johanes Kristiadi \n";
+
+// Heredoc
+
+echo <<<Johanes
+Ini adalah kalimat dengan menggunakan Heredoc 
+Pada heredoc tidak memerlukan spasi
+
+Johanes;
+
